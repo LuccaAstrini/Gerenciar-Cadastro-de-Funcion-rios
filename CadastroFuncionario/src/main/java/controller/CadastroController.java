e6b2c24a5448bb654fab678f045108db248f7203
Lucca Astrini <112909349+LuccaAstrini@users.noreply.github.com>
2025-03-25 22:31:35 -0300

    Cadastro

--- CadastroFuncionario/src/main/java/controller/CadastroController.java
+++ CadastroFuncionario/src/main/java/controller/CadastroController.java
@@ -2,14 +2,14 @@ package controller;
 
 
 import java.io.IOException;
+import model.Funcionario;
 import model.FuncionariosDAO;
-import view.Cadastro;
 
 public class CadastroController {
     
      public void grava(String cod, String nome, double salario) throws IOException{
         //Instanciar Produto
-        Cadastro cadastro = new cadastro(cod, nome, salario);
+        Funcionario cadastro = new Funcionario(cod, nome, salario);
         new FuncionariosDAO("produtos.txt").grava(cadastro);
     }
     
